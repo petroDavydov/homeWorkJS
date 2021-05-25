778cb008407209f9cf325ef009d26d7a81c10d18
petroDavydov <petrodavydov@gmail.com>
2021-05-25 23:28:25 +0300

    18 tasks done

--- main.js
+++ main.js
@@ -81,3 +81,75 @@ makeOrderMessage(10, 70, 200)
 
 // =======================
 
+
+function isValidPassword(password) {
+	const SAVED_PASSWORD = 'jqueryismyjam';
+	// Change code below this line
+	const isMatch = (password === SAVED_PASSWORD);
+
+	// Change code above this line
+	return (isMatch);
+};
+
+isValidPassword("mangodab3st");
+isValidPassword("kiwirul3z");
+isValidPassword("jqueryismyjam");
+
+
+//   ==================================
+
+
+function checkStorage(available, ordered) {
+	let message;
+	// Change code below this line
+	if (ordered > available) {
+		message = `Not enough goods in stock!`
+
+	} else {
+		message = `Order is processed, our manager will contact you.`
+	}
+
+	// Change code above this line
+	console.log(message);
+}
+checkStorage(100, 50);
+checkStorage(100, 130);
+checkStorage(200, 20);
+checkStorage(200, 150);
+
+
+
+// ========18=========
+
+
+function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+	let message;
+	// Change code below this line
+	let totalPrice = pricePerDroid * orderedQuantity;
+
+	if (totalPrice > customerCredits) {
+
+		message = "Insufficient funds!";
+
+	} else {
+
+		totalPrice = customerCredits - totalPrice
+
+
+		message = `You ordered ${orderedQuantity} droids, you have ${totalPrice} credits left`;
+
+	}
+
+	// Change code above this line
+	console.log(message);
+}
+
+makeTransaction(3000, 5, 23000);
+makeTransaction(1000, 3, 15000);
+makeTransaction(5000, 10, 8000);
+makeTransaction(2000, 8, 10000);
+makeTransaction(500, 10, 5000);
+
+
+// ==========
+
