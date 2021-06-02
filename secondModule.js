bce4b420785a17292f38bfebe784b8f059e15e07
petroDavydov <petrodavydov@gmail.com>
2021-06-02 14:16:51 +0300

    add 25-32 task

--- secondModule.js
+++ secondModule.js
@@ -1051,10 +1051,12 @@
 // 		}
 // 	}
 
-// 	// return word;
-// 	console.log (longestWord);
-// 	// Change code above this line
+// return word;
+// console.log (longestWord);
+// Change code above this line
 // }
+
+
 
 // console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
 // console.log(findLongestWord("Google do a roll"));
@@ -1159,48 +1161,143 @@
 
 // =26=
 
-function calculateTotalPrice(order) {
-	let total = 0;
+// function calculateTotalPrice(order) {
+// 	let total = 0;
+// 	// Change code below this line
+
+// 	for (const item of order) {
+// 	  total += item;
+// 	}
+
+// 	// Change code above this line
+// 	return total;
+//   }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// console.log(calculateTotalPrice([164, 48, 291]));
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+// =27=
+
+// function filterArray(numbers, value) {
+// 	// Change code below this line
+// 	const filteredNumbers = [];
+// 	let total  = 0;
+
+// 	for (let number of numbers) {
+// 	  total += number;
+
+// 	  if (number > value) {
+// 		filteredNumbers.push(number);
+// 	  }
+// 	}
+
+// 	return filteredNumbers;
+// 	// Change code above this line
+//   }
+
+
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+// console.log(filterArray());
+
+
+
+// =28=
+
+// Change code below this line
+// const a = 3 % 1;
+// const b = 4 % 3;
+// const c = 11 % 4;
+// const d = 12 % 7;
+// const e = 8 % 6 ;
+
+
+// console.log(a);//0
+// console.log(b);//1
+// console.log(c);//3
+// console.log(d);//5
+// console.log(e);//2
+
+
+// =29=
+
+// function getEvenNumbers(start, end) {
+// 	// Change code below this line
+// 	const arr = [];
+// 	for (let i = start; i <= end; i += 1) {
+// 		if (i % 2 === 0) {
+// 			arr.push(i)
+// 		}
+// 	}
+// 	return arr
+// 	// Change code above this line
+// }
+
+// console.log(getEvenNumbers(2, 5));
+// console.log(getEvenNumbers(3, 11));
+// console.log(getEvenNumbers(6, 12));
+// console.log(getEvenNumbers(8, 8));
+// console.log(getEvenNumbers(7, 7));
+// console.log(getEvenNumbers(7, 7));
+
+
+// =30=
+
+// const start = 6;
+// const end = 27;
+// let number;
+
+// for (let i = start; i < end; i += 1) {
+//   if (i % 5 === 0) {
+//     number = i;
+//     break;
+//   }
+// }
+
+// =31=
+
+// function findNumber(start, end, divisor) {
+// 	// Change code below this line
+// 	let number;
+
+// 	for (let i = start; i < end; i += 1) {
+// 		if (i % divisor === 0) {
+// 			number = i;
+// 			return number;
+// 		}
+// 	}
+
+// 	// Change code above this line
+// }
+
+// console.log(findNumber(2, 6, 5))
+// console.log(findNumber(8, 17, 3));
+// console.log(findNumber(6, 9, 4));
+
+
+// =32=
+
+return array.includes(value) ? true : false
+function includes(array, value) {
 	// Change code below this line
-  
-	for (let i = 0; i < order.length; i += 1) {
-	  total += order[i];
+	for (let num of array) {
+		if (num === value) {
+			return true
+		}
 	}
-  
+	return false
 	// Change code above this line
-	return total;
-  }
-  
-console.length(calculateTotalPrice([12, 85, 37, 4]));
-console.log(calculateTotalPrice([164, 48, 291]));
-console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(includes([1, 2, 3, 4, 5], 3));
+console.log(includes([1, 2, 3, 4, 5], 17));
+console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter"));
+console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus"));
 
